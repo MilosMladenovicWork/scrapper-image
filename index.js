a330e1f5429b77393e8d48c5cabde22dc34676b1
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-07-23 16:12:10 +0200

    changed bootstrap class from col-offset-1 to offset-1

--- index.js
+++ index.js
@@ -56,7 +56,7 @@ document.addEventListener('DOMContentLoaded', () => {
         imgBox.className = 'img-box container-fluid row row-cols-3'
         urls.forEach(url => {
           let image = document.createElement('img')
-          image.className = 'col shadow col-offset-1'
+          image.className = 'col shadow offset-1'
           image.setAttribute('src', url)
           document.querySelector('.img-box').appendChild(image)
         })
