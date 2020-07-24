fd44ee5b8679fc7775b7035edab9f4501c83cd95
Milos Mladenovic <milos.mladenovic.work@gmail.com>
2020-07-24 09:33:40 +0200

    added progress bar for images loading

--- index.js
+++ index.js
@@ -71,7 +71,17 @@ function fetchAndDisplayData(dataLoading, urls, e){
 
         let imagesLoaded = 0
 
-        Promise.all(allPics.filter(img => !img.complete).map(img => new Promise(resolve => { imagesLoaded += 1; console.log(imagesLoaded) ;img.onload = img.onerror = resolve; }))).then(() => {
+        let loadingBar = (width) => {
+          let progressContainer = document.createElement('div')
+          progressContainer.class = 'progress'
+          let progressBar = document.createElement('div')
+          progressBar.class = 'progress-bar progress-bar-striped progress-bar-animated'
+          progressBar.style = `${width}%`
+          progressContainer.appendChild(progressBar)
+          document.appendChild(progressContainer)
+        }
+
+        Promise.all(allPics.filter(img => !img.complete).map(img => new Promise(resolve => { imagesLoaded += 1 ;loadingBar(imagesLoaded / allPicsNum * 100);img.onload = img.onerror = resolve; }))).then(() => {
           console.log('images finished loading');
         }); 
 
